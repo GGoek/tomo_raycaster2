e8fe476fd118b52ae9ae6c20b3a254f775e19f77
Aleksandr Lizin <sogimu@nxt.ru>
2015-08-13 17:07:26 +0100

    colored brest. v1

--- src/shaders.js
+++ src/shaders.js
@@ -111,7 +111,7 @@
             "uniform float uSteps;",
 
             "//Acts like a texture3D using Z slices and trilinear filtering.",
-            "float getVolumeValue(vec3 volpos)",
+            "vec4 getVolumeValue(vec3 volpos)",
             "{",
             "    float s1Original, s2Original, s1, s2;",
             "    float dx1, dy1;",
@@ -143,17 +143,17 @@
             "    texpos2.x = dx2+(volpos.x/uSlicesOverX);",
             "    texpos2.y = dy2+(volpos.y/uSlicesOverY);",
 
-            "    float value1, value2;",
+            "    vec4 value1, value2;",
             "    bool value1Set = false, value2Set = false;",
 
             "    for (int x = 0; x < 16; x++) {",
             "        if(x == tex1Index) {",
-            "            value1 = texture2D(uSliceMaps[x],texpos1).x;",
+            "            value1 = texture2D(uSliceMaps[x],texpos1);",
             "            value1Set = true;",
             "        }",
 
             "        if(x == tex2Index) {",
-            "            value2 = texture2D(uSliceMaps[x],texpos2).x;",
+            "            value2 = texture2D(uSliceMaps[x],texpos2);",
             "            value2Set = true;",
             "        }",
 
@@ -174,11 +174,9 @@
             "    vec4 backColor = texture2D(uBackCoord,texC);",
 
             "    vec3 dir = backColor.rgb - frontColor.rgb;",
-            "    //dir /= length(dir);",
 
             "    vec4 vpos = frontColor;",
 
-            // "    vec3 Step = dir/uSteps;",
             "    vec3 Step = dir/uSteps;",
 
             "    vec4 accum = vec4(0, 0, 0, 0);",
@@ -198,26 +196,31 @@
             "            break;",
             "        }",
 
-            "        float gray_val = getVolumeValue(vpos.xyz);",
+            "        vec4 gray_val = getVolumeValue(vpos.xyz);",
 
-            "        if(gray_val < uMinGrayVal || gray_val > uMaxGrayVal) {",
-            "            colorValue = vec4(0.0);",
-            "        } else {",
-            "            if(biggest_gray_value < gray_val) {",
-            "               biggest_gray_value = gray_val;",
-            "            }",
+            "        if(gray_val.r < 0.01 && gray_val.g < 0.01 && gray_val.b < 0.01) {",
+            "           gray_val = vec4(0.0);",
+            "        }",
+
+
+            "        //if(gray_val < uMinGrayVal || gray_val > uMaxGrayVal) {",
+            "            //colorValue = vec4(0.0);",
+            "        //} else {",
+            "            //if(biggest_gray_value < gray_val) {",
+            "               //biggest_gray_value = gray_val;",
+            "            //}",
 
             "            if(uAbsorptionModeIndex == 0.0)",
                 "        {",
-                "            vec2 tf_pos;",
-                "            tf_pos.x = (gray_val - uMinGrayVal) / (uMaxGrayVal - uMinGrayVal);",
-                "            tf_pos.y = 0.5;",
+                "            //vec2 tf_pos;",
+                "            //tf_pos.x = (gray_val - uMinGrayVal) / (uMaxGrayVal - uMinGrayVal);",
+                "            //tf_pos.y = 0.5;",
 
-                "            colorValue = texture2D(uTransferFunction,tf_pos); " ,
+                "            //colorValue = texture2D(uTransferFunction,tf_pos); " ,
                 "            //colorValue = vec4(tf_pos.x, tf_pos.x, tf_pos.x, 1.0);",
 
-                "            sample.a = colorValue.a * opacityFactor;",
-                "            sample.rgb = colorValue.rgb * uColorVal;",
+                "            sample.a = gray_val.a * uOpacityVal;",
+                "            sample.rgb = gray_val.rgb * uColorVal;",
                 "            accum += sample;",
 
                 "            if(accum.a>=1.0)",
@@ -227,15 +230,15 @@
 
                 "        if(uAbsorptionModeIndex == 1.0)",
                 "        {",
-                "            vec2 tf_pos;",
-                "            tf_pos.x = (gray_val - uMinGrayVal) / (uMaxGrayVal - uMinGrayVal);",
-                "            tf_pos.y = 0.5;",
+                "            //vec2 tf_pos;",
+                "            //tf_pos.x = (gray_val - uMinGrayVal) / (uMaxGrayVal - uMinGrayVal);",
+                "            //tf_pos.y = 0.5;",
 
-                "            colorValue = texture2D(uTransferFunction,tf_pos); " ,
+                "            //colorValue = texture2D(uTransferFunction,tf_pos); " ,
                 "            //colorValue = vec4(tf_pos.x, tf_pos.x, tf_pos.x, 1.0);",
 
-                "            sample.a = colorValue.a * opacityFactor * (1.0 / uSteps);",
-                "            sample.rgb = (1.0 - accum.a) * colorValue.rgb * sample.a * lightFactor;",
+                "            sample.a = gray_val.a * opacityFactor * (1.0 / uSteps);",
+                "            sample.rgb = (1.0 - accum.a) * gray_val.rgb * sample.a * lightFactor;",
                 "            accum += sample;",
 
                 "            if(accum.a>=1.0)",
@@ -245,20 +248,20 @@
 
                 "        if(uAbsorptionModeIndex == 2.0)",
                 "        {",
-                "            vec2 tf_pos;",
-                "            tf_pos.x = (biggest_gray_value - uMinGrayVal) / (uMaxGrayVal - uMinGrayVal);",
-                "            tf_pos.y = 0.5;",
+                "            //vec2 tf_pos;",
+                "            //tf_pos.x = (biggest_gray_value - uMinGrayVal) / (uMaxGrayVal - uMinGrayVal);",
+                "            //tf_pos.y = 0.5;",
                 
-                "            colorValue = texture2D(uTransferFunction,tf_pos); " ,
+                "            //colorValue = texture2D(uTransferFunction,tf_pos); " ,
                 "            //colorValue = vec4(tf_pos.x, tf_pos.x, tf_pos.x, 1.0);",
-                "            sample.a = colorValue.a * opacityFactor;",
-                "            sample.rgb = colorValue.rgb * uColorVal;",
+                "            sample.a = gray_val.a * opacityFactor;",
+                "            sample.rgb = gray_val.rgb * uColorVal;",
 
                 "            accum = sample;",
 
                 "        }",
 
-            "        }",
+            "        //}",
 
             "        //advance the current position",
             "        vpos.xyz += Step;",
